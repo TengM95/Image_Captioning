dd92876fb1fbb28e6333a77edf4dacb49f6d99f6
Teng Ma <teng-m@outlook.com>
2019-12-06 17:44:36 -0800

    update

--- datasets.py
+++ datasets.py
@@ -5,7 +5,8 @@ import json
 import os
 import numpy as np
 import json
-from scipy.misc import imread, imresize
+from imageio import imread
+from skimage.transform import resize
 from tqdm import tqdm
 from collections import Counter
 from random import seed, choice, sample
@@ -117,7 +118,7 @@ def create_input_files(dataset, karpathy_json_path, image_folder, captions_per_i
                 if len(img.shape) == 2:
                     img = img[:, :, np.newaxis]
                     img = np.concatenate([img, img, img], axis=2)
-                img = imresize(img, (256, 256))
+                img = resize(img, (256, 256))
                 img = img.transpose(2, 0, 1)
                 assert img.shape == (3, 256, 256)
                 assert np.max(img) <= 255
